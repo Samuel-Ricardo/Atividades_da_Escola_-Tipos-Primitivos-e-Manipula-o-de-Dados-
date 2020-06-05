3c981334cba9eba04418640237c2e8356dc031bb
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-06-05 14:22:46 -0300

    activity has been concluded

--- src/EstruturaDeRepetiçao/ContarNumeros.java
+++ src/EstruturaDeRepetiçao/ContarNumeros.java
@@ -5,19 +5,26 @@
  */
 package EstruturaDeRepetiçao;
 
-public class InteirosDepoisDe100 {
-    
+import java.util.Scanner;
+
+/**
+ *
+ * @author Samuel
+ */
+public class ContarNumeros {
+
     public static void main(String[] args) {
-        
-        int cont;
-        
-        for (cont = 101; cont < 111; cont++) {
-            
+
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Digite um número");
+
+        int n = tec.nextInt();
+
+        for (int cont = 1; cont < n + 1; cont++) {
+
             System.out.println(cont);
-            
+
         }
-
     }
-    
 }
-
