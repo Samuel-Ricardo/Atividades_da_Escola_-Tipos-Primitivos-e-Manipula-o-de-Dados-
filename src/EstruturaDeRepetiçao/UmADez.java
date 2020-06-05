047ac92b3d44af6daefe164107b51e9b897f9b7c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-06-05 13:55:48 -0300

    activity has been concluded

---
+++ src/EstruturaDeRepetiçao/UmADez.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EstruturaDeRepetiçao;
+
+/**
+ *
+ * @author Samuel
+ */
+public class UmADez {
+    
+    public static void main(String[] args) {
+        
+        int cont;
+        
+        for (cont = 1; cont<11; cont++) {
+            
+            System.out.println(cont);
+            
+        }
+
+    }
+    
+}
