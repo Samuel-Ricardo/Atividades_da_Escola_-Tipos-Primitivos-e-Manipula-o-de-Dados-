1c06679491571bb158a7562f860f8957a64d472e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2020-06-05 14:32:19 -0300

    activity has been concluded

---
+++ src/EstruturaDeRepetiçao/TabuadaDoOito.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EstruturaDeRepetiçao;
+
+/**
+ *
+ * @author Samuel
+ */
+public class TabuadaDoOito {
+    
+      public static void main(String[] args) {
+
+        for (int cont = 0; cont < 11 ; cont++) {
+
+            System.out.println("8 x "+cont+" = "+cont*8);
+
+        }
+    } 
+}
